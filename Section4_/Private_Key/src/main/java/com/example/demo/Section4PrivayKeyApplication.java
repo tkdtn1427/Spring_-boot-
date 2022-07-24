ef35d580a77e4a90f90c949290686384d96b885f
SangSoo <tkdtn1427@gmail.com>
2022-07-24 22:59:45 +0900

    Spring_coding

---
+++ Section4_/Private_Key/src/main/java/com/example/demo/Section4PrivayKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4PrivayKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4PrivayKeyApplication.class, args);
+	}
+
+}
---
+++ Section4_/Private_Key/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    public Member findByUsername(String username);
+}
